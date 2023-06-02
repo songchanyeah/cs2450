e87c5625d87800f38e92d6888101544eb6e37ad0
Jordan Convey <85854964+j-convey@users.noreply.github.com>
2023-06-02 12:00:37 -0600

    Add files via upload
    main

---
+++ main.py
@@ -0,0 +1,22 @@
+class UVSim:
+    def __init__(self):
+        self.memory = [0] * 100
+        self.accumulator = 0
+        self.instruction_counter = 0
+        self.instruction_register = 0
+        self.operation_code = 0
+        self.operand = 0
+
+    def load(self, filename):
+        pass
+
+    def execute(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
