19bb705670f1ad37fd4adb231f78550c247c6b1f
DESKTOP-PJE6QMO\cyhso <songverlining94@gmail.com>
2023-06-02 12:14:01 -0600

    pseudocode

--- main.py
+++ main.py
@@ -21,3 +21,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# address 00 ~ 99
+# list
+
+# put values in the list and parse the first character
+# if it is not minus,
+#   if it is plus sign,
+#   parse the first two characters after the plus sign
+#   else, parse the first two characters and check if they are instructions
+#
+# else they are data so they can be minus
+#
+# when each index of the list is executed, put it in a variable called accumulator
