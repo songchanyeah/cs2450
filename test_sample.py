090a43735464ca8a716cc7b0048cca10845374fd
Daniel Wilsher <danielwilsher@dhcp-161-28-253-193.ad.uvu.edu>
2023-06-09 13:11:35 -0600

    did some merges

--- test_sample.py
+++ test_sample.py
@@ -1,43 +1,50 @@
 import pytest
+
 from main import main, UVSim
 
-#To run these tests, open a terminal and run "pytest" in the terminal to check if tests pass.
+# To run these tests, open a terminal and run "pytest" in the terminal to check if tests pass.
 
 
-#tutorial function for test number 1
+# tutorial function for test number 1
 def func(x):
     return x + 1
 
 
-def test_answer():
-    assert func(3) == 4
 
-#tutorial function and test number 2
+# tutorial function and test number 2
+
+
 def f():
     raise SystemExit(1)
 
+
 def test_fetch():
     sim = UVSim()
-    sim.memory[0] = 1002 # opcode 10 and operand 01
-    sim.memory[1] = 2130 # opcode 21 and operand 30
-    sim.memory[2] = 4300 # opcode 43 and operand 00
-    sim.memory[3] = 0000 # opcode 00 and operand 00
-    sim.memory[4] = 0000 # opcode 00 and operand 00
-    sim.memory[5] = 0000 # opcode 00 and operand 00
-    
+    sim.memory[0] = 1002  # opcode 10 and operand 01
+    sim.memory[1] = 2130  # opcode 21 and operand 30
+    sim.memory[2] = 4300  # opcode 43 and operand 00
+    sim.memory[3] = 0000  # opcode 00 and operand 00
+    sim.memory[4] = 0000  # opcode 00 and operand 00
+    sim.memory[5] = 0000  # opcode 00 and operand 00
+
     sim.fetch()
     assert sim.operation_code == 10
     assert sim.operand == 2
+
 
 def test_mytest():
     with pytest.raises(SystemExit):
         f()
 
-#function for testing main in main.py returns a 1 integer
+# function for testing main in main.py returns a 1 integer
+
+
 def test_main():
     assert main() == 1
 
-#testing UVSim class constructor
+# testing UVSim class constructor
+
+
 def test_UVSim():
     uvsim = UVSim()
 
