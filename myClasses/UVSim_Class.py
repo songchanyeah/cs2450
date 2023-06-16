3267b30f39070a8eb8b5938c26b4280718b3a008
j-convey <arg0nath@protonmail.com>
2023-06-16 11:07:31 -0600

    execute function issues
    Fixed issue with execution not looping. Fixed case issues. Added instruction counter

--- myClasses/UVSim_Class.py
+++ myClasses/UVSim_Class.py
@@ -24,7 +24,7 @@ class UVSim:
                 sign = -1 if line[0] == '-' else 1
                 # Convert the number to an integer and store it in memory
                 self.memory[i] = int(line[1:]) * sign
-        print(self.memory)
+        #print(self.memory)
 
     def fetch(self):
         '''Fetch Instructions From Memory'''
@@ -39,50 +39,68 @@ class UVSim:
         filename = input("Please enter the name of your text file to be read in: ")
         # use filename "Test Files/Test1.txt" to run
         self.load(filename)
-        match self.operation_code:
-            case 10:  # READ
-                '''Gets user input and stores it in memory specified by the operand value'''
-                self.memory[self.operand] = int(
-                    input(("Type the value to be READ in console: ")))
-            case 11:  # WRITE
-                # Write a word from a specific location in memory to screen
-                print(self.memory[self.operand])
-            case 20:  # LOAD
-                '''Load a word from a specific location in memory into the accumulator'''
-                #self.accumulator = self.register[self.register_index]
-            case 21:  # STORE
-                '''Store a word from the accumulator into a specific location in memory'''
-                #self.register[self.register_index] = self.accumulator
-            case 30:  # ADD
-                '''Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                #self.accumulator = self.accumulator + \
-                   # self.register[self.register_index]
-            case 31:  # SUBTRACT
-                '''Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)'''
-               # self.accumulator = self.accumulator - \
-                    #self.register[self.register_index]
-            case 32:  # DIVIDE
-                '''Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator)'''
-                #self.accumulator = self.accumulator / \
-                    #self.register[self.register_index]
-            case 33:  # MULTIPLY
-                '''Multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                #self.accumulator = self.accumulator * \
-                    #self.register[self.register_index]
-            case 40:  # BRANCH
-                '''Branch to a specific location in memory'''
-                self.instruction_counter = self.operand
-            case 41:  # BRANCHNEG
-                '''Branch to a specific location in memory if the accumulator is negative'''
-                if self.accumulator < 0:
+        while True:
+            self.fetch()
+            match self.operation_code:
+                case 10:  # READ
+                    '''Gets user input and stores it in memory specified by the operand value'''
+                    try:
+                        user_input = int(input("Enter an integer between -9999 and 9999: "))
+                        if not -9999 <= user_input <= 9999:
+                            raise ValueError("Input must be between -9999 and 9999")
+                        self.memory[self.operand] = user_input
+                    except ValueError as e:
+                        print(e)
+                case 11:  # WRITE
+                    '''Write a word from a specific location in memory to screen'''
+                    print(f"Memory[{self.operand}]: {self.memory[self.operand]}")
+
+                case 20:  # LOAD
+                    '''Load a word from a specific location in memory into the accumulator'''
+                    self.accumulator = self.memory[self.operand]
+
+                case 21:  # STORE
+                    '''Store a word from the accumulator into a specific location in memory'''
+                    self.memory[self.operand] = self.accumulator
+
+                case 30:  # ADD
+                    '''Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
+                    self.accumulator += self.memory[self.operand]
+
+                case 31:  # SUBTRACT
+                    '''Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)'''
+                    self.accumulator -= self.memory[self.operand]
+
+                case 32:  # DIVIDE
+                    '''Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator)'''
+                    if self.memory[self.operand] != 0:
+                        self.accumulator /= self.memory[self.operand]
+                    else:
+                        raise ZeroDivisionError("Cannot divide by zero")
+                    
+                case 33:  # MULTIPLY
+                    '''Multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
+                    self.accumulator *= self.memory[self.operand]
+
+                case 40:  # BRANCH
+                    '''Branch to a specific location in memory'''
                     self.instruction_counter = self.operand
-            case 42:  # BRANCHZERO
-                '''Branch to a specific location in memory if the accumulator is zero'''
-                if self.accumulator == 0:
-                    self.instruction_counter = self.operand
-            case 43:  # HALT
-                '''Halt the program'''
-                print("Program halted.")
-                return "Program halted."
+
+                case 41:  # BRANCHNEG
+                    '''Branch to a specific location in memory if the accumulator is negative'''
+                    if self.accumulator < 0:
+                        self.instruction_counter = self.operand
+
+                case 42:  # BRANCHZERO
+                    '''Branch to a specific location in memory if the accumulator is zero'''
+                    if self.accumulator == 0:
+                        self.instruction_counter = self.operand
+                        
+                case 43:  # HALT
+                    '''Halt the program'''
+                    print("Program halted.")
+                    return "Program halted."
+                    
+            self.instruction_counter += 1
             
             
