bdd0376c60ff3ed1ec8cc1b247f3497cbfe4f15d
songchanyeah <songverlining94@gmail.com>
2023-06-19 16:27:41 -0600

    Added self.fetch() in each cases so that it will update instruction counter, which prevents while loop running infinitely. Deleted the self.operand_code = 43 which was there temporarily to prevent code from running infinitely. Print functions are not required but there to help understand what is in the memory. They are sandwiched by ###### which can be deleted later.

--- myClasses/UVSim_Class.py
+++ myClasses/UVSim_Class.py
@@ -12,12 +12,15 @@ class UVSim:
         self.operand = 0
         self.register = 0 #This will need to be fixed, there was no class object being referenced here.
         self.register_index = 0 #This will need to be fixed, there was no class object being referenced here.
+        self.operation_codes = [10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43]
 
     def load(self, filename):
         '''Load Instructions Into Memory'''
         current_dir = os.getcwd()
-        current_dir = current_dir + "\Test Files\""
-        relative_path += filename
+        current_dir = current_dir + "\Test Files\\"
+        relative_path = current_dir + filename
+
+        # print(relative_path)
 
         if not os.path.exists(relative_path):
             print(f"File '{relative_path}' does not exist.")
@@ -29,7 +32,9 @@ class UVSim:
                 sign = -1 if line[0] == '-' else 1
                 # Convert the number to an integer and store it in memory
                 self.memory[i] = int(line[1:]) * sign
-        #print(self.memory)
+        ###################        
+        print("self.memory: ", self.memory)
+        ###################
 
     def fetch(self):
         '''Fetch Instructions From Memory'''
@@ -38,6 +43,7 @@ class UVSim:
         self.operation_code = self.instruction_register // 100
         # Discard first two digits so we just have the last two (operand)
         self.operand = self.instruction_register % 100
+        self.instruction_counter += 1
 
     def execute(self):
         '''Execute Instructions'''
@@ -45,38 +51,49 @@ class UVSim:
         # use filename "Test Files/Test1.txt" to run
         self.load(filename)
 
+        self.fetch()
+        ###################
+        print("operation code: ", self.operation_code)
+        print("operand: ", self.operand)
+        ###################
         while self.operation_code != 43:
             match self.operation_code:
                 case 10:  # READ
                     '''Gets user input and stores it in memory specified by the operand value'''
                     user_input = int(input("Enter an integer between -9999 and 9999: "))
-                    # try:
-                    if not -9999 <= user_input <= 9999:
-                        raise ValueError("Input must be between -9999 and 9999")
-                        self.memory[self.operand] = user_input
-                    else:
+                    try:
+                        if not -9999 <= user_input <= 9999:
+                            raise ValueError("Input must be between -9999 and 9999")
+                        else:
+                            self.memory[self.operand] = user_input
+                    except ValueError as e:
                         print(e)
-                    # except ValueError as e:
-                        # print(e)
+                    self.fetch()
+
                 case 11:  # WRITE
                     '''Write a word from a specific location in memory to screen'''
                     print(f"Memory[{self.operand}]: {self.memory[self.operand]}")
+                    self.fetch()
 
                 case 20:  # LOAD
                     '''Load a word from a specific location in memory into the accumulator'''
                     self.accumulator = self.memory[self.operand]
+                    self.fetch()
 
                 case 21:  # STORE
                     '''Store a word from the accumulator into a specific location in memory'''
                     self.memory[self.operand] = self.accumulator
+                    self.fetch()
 
                 case 30:  # ADD
                     '''Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
                     self.accumulator += self.memory[self.operand]
+                    self.fetch()
 
                 case 31:  # SUBTRACT
                     '''Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)'''
                     self.accumulator -= self.memory[self.operand]
+                    self.fetch()
 
                 case 32:  # DIVIDE
                     '''Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator)'''
@@ -84,31 +101,39 @@ class UVSim:
                         self.accumulator /= self.memory[self.operand]
                     else:
                         raise ZeroDivisionError("Cannot divide by zero")
-                    
+                    self.fetch()
+
                 case 33:  # MULTIPLY
                     '''Multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
                     self.accumulator *= self.memory[self.operand]
+                    self.fetch()
 
                 case 40:  # BRANCH
                     '''Branch to a specific location in memory'''
                     self.instruction_counter = self.operand
-                    
+                    self.fetch()
+
                 case 41:  # BRANCHNEG
                     '''Branch to a specific location in memory if the accumulator is negative'''
                     if self.accumulator < 0:
                         self.instruction_counter = self.operand
+                    self.fetch()
 
                 case 42:  # BRANCHZERO
                     '''Branch to a specific location in memory if the accumulator is zero'''
                     if self.accumulator == 0:
                         self.instruction_counter = self.operand
+                    self.fetch()
+
                 case 43:  # HALT
                     '''Halt the program'''
                     print("Program halted.")
                     self.operation_code = 43
-                    return self.operation_code 
+                    self.fetch()
 
-            self.operation_code = 43 #DO NOT REMOVE YET, this breaks the while loop for the case matches
-                
+                    return self.operation_code 
+        ###################
+        print("self.memory: ", self.memory)
+        ###################
             
             
