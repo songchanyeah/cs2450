f0c450b4329df718d140d69dd52f2af19b5ae52b
songchanyeah <songverlining94@gmail.com>
2023-07-15 13:06:33 -0600

    gui updated (milestone 3)

--- main.py
+++ main.py
@@ -1,10 +1,133 @@
+import tkinter as tk
+from tkinter import *
+import os
 from myClasses.UVSim_Class import UVSim
 
-#this file is to run the main python program.
+class UVSimGUI(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.uvsim = UVSim(self.output_instruction_process, self.output_accumulator)
+        self.create_widgets()
+        self.user_input_ready = tk.BooleanVar(value=False)
 
-def main():
-    uvsim = UVSim()
-    uvsim.execute()
+    def create_widgets(self):
+        ##### Filename #####
+        self.filename_label = tk.Label(self.master, text="Enter the filename:")
+        self.filename_label.grid(row=0, column=0, sticky=W)
+
+        self.entry1 = tk.Entry(self.master)
+        self.entry1.grid(row=0, column=2, sticky=W)
+
+        self.execute_button = tk.Button(self.master, text="Execute", command=self.execute_program)
+        self.execute_button.grid(row=0, column=2, sticky=E)
+        
+        self.output_label = tk.Label(self.master, text="")
+        self.output_label.grid(row=0, column=3, sticky=W)
+
+        ### < For UVSim Class > ###
+        ##### User Input for Read Operation #####
+        self.user_input_for_read_label = tk.Label(self.master, text="Enter an integer between -9999 and 9999:")
+        self.user_input_for_read_label.grid(row=1, column=0, sticky=W)
+
+        self.user_input_entry = tk.Entry(self.master)
+        self.user_input_entry.grid(row=1, column=2, sticky=W)
+
+        self.user_input_button = tk.Button(self.master, text="Enter", command=self.get_user_input)
+        self.user_input_button.grid(row=1, column=2, sticky=E)
+
+        ### < For UVSim Class > ###
+        ##### Accumulator #####
+        self.accumulator_label = tk.Label(self.master, text="Accumulator: ")
+        self.accumulator_label.grid(row=2, column=0, sticky=W)
+
+        self.accumulator_entry = tk.Entry(self.master)
+        self.accumulator_entry.grid(row=2, column=2, sticky=W)
+
+        ##### Memory #####
+        self.listbox_for_memory = tk.Listbox(self.master)
+        self.listbox_for_memory.grid(row=3, column=0, sticky=tk.NSEW)
+
+        self.scrollbar_for_memory = tk.Scrollbar(self.master, orient=tk.VERTICAL)
+        self.scrollbar_for_memory.grid(row=3, column=1, sticky=tk.NS)
+
+        self.listbox_for_memory.config(yscrollcommand=self.scrollbar_for_memory.set)
+        self.scrollbar_for_memory.config(command=self.listbox_for_memory.yview)
+
+        ### < For UVSim Class > ###
+        ##### Instructions Run #####
+        self.output_text = tk.Text(self.master, height=10, width=50)
+        self.output_text.grid(row=3, column=2)
+
+
+    def execute_program(self):
+        justfilename = self.entry1.get()
+        current_dir = os.getcwd()
+        current_dir = current_dir + "\Test Files\\"
+        filename = current_dir + justfilename
+
+        # if filename:
+        if os.path.exists(filename):
+            self.output_label.config(text = justfilename + " loaded successfully.")
+            self.entry1.delete(0, tk.END)
+            print(self.uvsim.memory)
+            self.uvsim.execute(filename)
+            self.display_memory()
+        else:
+            self.output_label.config(text = justfilename + " does not exist.")
+            self.entry1.delete(0, tk.END)
+
+
+    def display_memory(self):
+        for item in self.uvsim.memory:
+            self.listbox_for_memory.insert(tk.END, item)
+
+    ##### Passed to UVsim class in UVSim_Class.py #####
+    def output_instruction_process(self, text):
+        self.output_text.insert(tk.END, text + "\n")
+
+    # def output_user_input_for_read(self, text):
+    #     self.user_input_entry.insert(tk.END, text)
+
+    def output_accumulator(self, text):
+        self.accumulator_entry.insert(tk.END, text)
+
+    def get_user_input(self):
+        self.user_input_from_gui = int(self.user_input_entry.get())
+        self.user_input_entry.delete(0, tk.END)
+        self.uvsim.user_input_from_gui = self.user_input_from_gui  # Set the user_input_from_gui variable in UVSim
+
+        return self.user_input_from_gui
+
+    
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = UVSimGUI(root)
+    app.mainloop()
+
+
+
+
+# self.load_button = tk.Button(self.master, text="Load File", command=self.load_file)
+# self.load_button.pack()
+
+# def load_file(self):
+#     filename = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+#     if filename:
+#         self.entry.delete(0, tk.END)
+#         self.entry.insert(tk.END, filename)
+#         self.uvsim.load(filename)
+#         self.output_text.insert(tk.END, "File loaded successfully.\n")
+
+# from myClasses.UVSim_Class import UVSim
+
+# #this file is to run the main python program.
+
+# def main():
+#     uvsim = UVSim()
+#     uvsim.execute()
+
+# if __name__ == "__main__":
+#     main()
+
--- myClasses/UVSim_Class.py
+++ myClasses/UVSim_Class.py
@@ -2,13 +2,18 @@ import os
 
 class UVSim:
 
-    def __init__(self):
+    def __init__(self, output_function, output_accumulator):
         self.memory = [0] * 100
         self.accumulator = 0
         self.instruction_counter = 0
         self.instruction_register = 0
         self.operation_code = 0
         self.operand = 0
+        self.output_instruction_process = output_function
+        self.output_accumulator = output_accumulator
+        # self.output_user_input = output_user_input
+        # self.user_input_from_gui = user_input
+        self.user_input_from_gui = 0
 
     def load(self, filename):
         '''Load Instructions Into Memory'''
@@ -22,7 +27,8 @@ class UVSim:
                 sign = -1 if line[0] == '-' else 1
                 # Convert the number to an integer and store it in memory
                 self.memory[i] = int(line[1:]) * sign
-        print(self.memory)
+        # print(self.memory)
+        
 
     def fetch(self):
         '''Fetch Instructions From Memory'''
@@ -32,27 +38,24 @@ class UVSim:
         # Discard first two digits so we just have the last two (operand)
         self.operand = self.instruction_register % 100
 
-    def execute(self):
+    def execute(self, filename):
         '''Execute Instructions'''
-        filename = input("Please enter the name of your text file to be read in: ")
-        current_dir = os.getcwd()
-        current_dir = current_dir + "/Test Files/"
-        relative_path = current_dir + filename
         
-        self.load(relative_path)
+        self.load(filename)
         
         while True:
             self.fetch()
             if self.operation_code == 43: # HALT
                 '''Halt the program'''
-                print("Program halted.")
+                self.output_instruction_process("Program halted.")
                 break  
             
             elif self.operation_code == 10:  # READ
                 '''Gets user input and stores it in memory specified by the operand value'''
-                print("Triggered READ")
+                self.output_instruction_process("Triggered READ, operand 10")
                 try:
-                    user_input = int(input("Enter an integer between -9999 and 9999: "))
+                    # user_input = int(input("Enter an integer between -9999 and 9999: "))
+                    user_input = self.user_input_from_gui
                     if not -9999 <= user_input <= 9999:
                         raise ValueError("Input must be between -9999 and 9999")
                     self.memory[self.operand] = user_input
@@ -61,32 +64,33 @@ class UVSim:
                         
             elif self.operation_code == 11: # WRITE
                 '''Write a word from a specific location in memory to screen'''
-                print("Triggered WRITE")
+                self.output_instruction_process("Triggered WRITE, operand 11")
                 print(f"Memory[{self.operand}]: {self.memory[self.operand]}")
 
             elif self.operation_code == 20:  # LOAD
                 '''Load a word from a specific location in memory into the accumulator'''
-                print("Triggered LOAD")
+                self.output_instruction_process("Triggered LOAD, operand 20")
                 self.accumulator = self.memory[self.operand]
+                self.output_accumulator(self.accumulator)
 
             elif self.operation_code == 21:  # STORE
                 '''Store a word from the accumulator into a specific location in memory'''
-                print("Triggered STORE")
+                self.output_instruction_process("Triggered STORE, operand 21")
                 self.memory[self.operand] = self.accumulator
 
             elif self.operation_code == 30:  # ADD
                 '''Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                print("Triggered ADD")
+                self.output_instruction_process("Triggered ADD, operand 30")
                 self.accumulator += self.memory[self.operand]
 
             elif self.operation_code == 31: # SUBTRACT
                 '''Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)'''
-                print("Triggered SUBTRACT")
+                self.output_instruction_process("Triggered SUBTRACT, operand 31")
                 self.accumulator -= self.memory[self.operand]
 
             elif self.operation_code == 32:  # DIVIDE
                 '''Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator)'''
-                print("Triggered DIVIDE")
+                self.output_instruction_process("Triggered DIVIDE, operand 32")
                 if self.memory[self.operand] != 0:
                     self.accumulator /= self.memory[self.operand]
                 else:
@@ -94,29 +98,30 @@ class UVSim:
                 
             elif self.operation_code == 33:# MULTIPLY
                 '''Multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                print("Triggered MULTIPLY")
+                self.output_instruction_process("Triggered MULTIPLY, operand 33")
                 self.accumulator *= self.memory[self.operand]
 
             elif self.operation_code == 40:# BRANCH
                 '''Branch to a specific location in memory'''
-                print("Triggered BRANCH")
+                self.output_instruction_process("Triggered BRANCH, operand 40")
                 self.instruction_counter = self.operand
                 
             elif self.operation_code == 41: # BRANCHNEG
                 '''Branch to a specific location in memory if the accumulator is negative'''
-                print("Triggered BRANCHNEG")
+                self.output_instruction_process("Triggered BRANCHNEG, operand 41")
                 if self.accumulator < 0:
                     self.instruction_counter = self.operand
                     continue
                 
             elif self.operation_code == 42: # BRANCHZERO
                 '''Branch to a specific location in memory if the accumulator is zero'''
-                print("Triggered BRANCHZERO")
+                self.output_instruction_process("Triggered BRANCHZERO, operand 42")
                 if self.accumulator == 0:
                     self.instruction_counter = self.operand
                     continue
             else:
-                print("Invalid operation code.")
+                self.output_instruction_process("Invalid operation code.")
             
             self.instruction_counter += 1
-            #print(self.memory)
+
+    
