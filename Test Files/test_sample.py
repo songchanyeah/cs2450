550a636b6f3a33c4ac1402dc62451612e4b500f6
j-convey <85854964+j-convey@users.noreply.github.com>
2023-08-04 20:16:31 -0600

    gui fixes

--- Test Files/test_sample.py
+++ Test Files/test_sample.py
@@ -1,15 +1,17 @@
 import pytest
 from myClasses.UVSim_Class import UVSim
+import os
+import unittest
 
 # Test that checks the fetch function UT1
 def test_fetch():
     sim = UVSim()
-    sim.memory[0] = 1002  # opcode 10 and operand 02
-    sim.memory[1] = 2130  # opcode 21 and operand 30
-    sim.memory[2] = 4300  # opcode 43 and operand 00
-    sim.memory[3] = 0000  # opcode 00 and operand 00
-    sim.memory[4] = 0000  # opcode 00 and operand 00
-    sim.memory[5] = 0000  # opcode 00 and operand 00
+    sim.memory[0] = 01002  # opcode 010 and operand 02
+    sim.memory[1] = 02130  # opcode 021 and operand 30
+    sim.memory[2] = 04300  # opcode 043 and operand 00
+    sim.memory[3] = 00000  # opcode 000 and operand 00
+    sim.memory[4] = 0000  # opcode 000 and operand 00
+    sim.memory[5] = 0000  # opcode 000 and operand 00
 
     sim.fetch()
     assert sim.operation_code == 10
@@ -19,11 +21,11 @@ def test_fetch():
 def test_fetch_instruction():
     uvsim = UVSim()
 
-    uvsim.memory = [1007, 2010, 3018, 2119, 4017]
+    uvsim.memory = [01007, 02010, 03018, 02119, 04017]
     uvsim.instruction_counter = 2
     uvsim.fetch()
 
-    assert uvsim.instruction_register == 3018
+    assert uvsim.instruction_register == 03018
     assert uvsim.operation_code == 30
     assert uvsim.operand == 18
 
@@ -31,7 +33,7 @@ def test_fetch_instruction():
 def test_UVSim_Constructor():
     uvsim = UVSim()
 
-    assert uvsim.memory == [0] * 100
+    assert uvsim.memory == [0] * 250
     assert uvsim.accumulator == 0
     assert uvsim.instruction_counter == 0
     assert uvsim.instruction_register == 0
@@ -43,7 +45,7 @@ def test_UVSim_Constructor():
 def test_UVSim_Memory():
     uvsim = UVSim()
 
-    assert uvsim.memory == [0] * 100
+    assert uvsim.memory == [0] * 250
     assert isinstance(uvsim.memory, list)
 
 #UT5
@@ -51,7 +53,7 @@ def test_UVSim_Memory_Range_Error():
     uvsim = UVSim()
 
     with pytest.raises(IndexError):
-        value = uvsim.memory[101]
+        value = uvsim.memory[251]
 
 #UT6
 def test_UVSim_Accumulator():
@@ -88,12 +90,11 @@ def test_UVSim_Operand():
     assert uvsim.operand == 0
     assert isinstance(uvsim.operand, int)
 
-
 # Test that checks the load function UT11
 def test_load():
     sim = UVSim()
     sim.load('Test Files/Test1.txt')
-    expected_memory = [+1007, +1008, +2007, +2008, +2109, +1109, +4300, +0000, +0000, +0000, -99999]
+    expected_memory = [+01007, +01008, +02007, +02008, +02109, +01109, +04300, +00000, +00000, +00000, -099999]
 
     assert sim.memory[:len(expected_memory)] == expected_memory, f'Expected {expected_memory}, but got {sim.memory[:len(expected_memory)]}'
 
@@ -110,12 +111,12 @@ def test_Load_Wrong_Input(capsys):
 def test_load_empty_file():
     sim = UVSim()
     sim.load('Test File/empty.txt')
-    assert sim.memory == [0] * 100
+    assert sim.memory == [0] * 250
 
 #UT15
 def test_execute_op_code_11(capsys):
     sim = UVSim()
-    sim.memory[sim.operand] = 5678
+    sim.memory[sim.operand] = 05678
     sim.write_output()
     captured = capsys.readouterr()
     assert captured.out.strip().endswith("Memory[0]: 5678")
@@ -124,24 +125,24 @@ def test_execute_op_code_11(capsys):
 #UT16
 def test_execute_op_code_20():
     sim = UVSim()
-    sim.memory[sim.operand] = 4321
+    sim.memory[sim.operand] = 04321
     sim.load_word()
-    assert sim.accumulator == 4321
+    assert sim.accumulator == 04321
 
 #UT17
 def test_execute_op_code_21():
     sim = UVSim()
-    sim.accumulator = 8765
+    sim.accumulator = 08765
     sim.store_word()
-    assert sim.memory[sim.operand] == 8765
+    assert sim.memory[sim.operand] == 08765
 
 #UT18
 def test_execute_op_code_30():
     sim = UVSim()
-    sim.accumulator = 100
-    sim.memory[sim.operand] = 200
+    sim.accumulator = 250
+    sim.memory[sim.operand] = 0200
     sim.add_word()
-    assert sim.accumulator == 300
+    assert sim.accumulator == 0300
 
 #UT19
 def test_execute_op_code_31():
@@ -154,7 +155,7 @@ def test_execute_op_code_31():
 #UT20
 def test_execute_op_code_32():
     sim = UVSim()
-    sim.accumulator = 100
+    sim.accumulator = 250
     sim.memory[sim.operand] = 5
     sim.divide_word()
     assert sim.accumulator == 20
@@ -162,7 +163,7 @@ def test_execute_op_code_32():
 #UT21
 def test_execute_op_code_32_with_zero():
     sim = UVSim()
-    sim.accumulator = 100
+    sim.accumulator = 250
     sim.memory[sim.operand] = 0
     with pytest.raises(ZeroDivisionError):
         sim.divide_word()
@@ -170,7 +171,7 @@ def test_execute_op_code_32_with_zero():
 #UT22
 def test_execute_op_code_33():
     sim = UVSim()
-    sim.accumulator = 50
+    sim.accumulator = 250
     sim.memory[sim.operand] = 4
     sim.multiply_word()
     assert sim.accumulator == 200
@@ -178,7 +179,7 @@ def test_execute_op_code_33():
 #UT22
 def test_execute_op_code_40():
     sim = UVSim()
-    sim.operand = 10
+    sim.operand = 010
     sim.branch_memory()
     assert sim.instruction_counter == sim.operand
 
@@ -186,14 +187,14 @@ def test_execute_op_code_40():
 def test_execute_op_code_41():
     sim = UVSim()
     sim.accumulator = -5
-    sim.operand = 10
+    sim.operand = 010
     sim.branch_negative()
     assert sim.instruction_counter == sim.operand
 
 def test_execute_op_code_41_no_branch():
     sim=UVSim()
     sim.accumulator = 5
-    sim.operand = 10
+    sim.operand = 010
     sim.branch_negative()
     assert sim.instruction_counter != sim.operand
 
@@ -201,14 +202,14 @@ def test_execute_op_code_41_no_branch():
 def test_execute_op_code_42():
     sim = UVSim()
     sim.accumulator = 0
-    sim.operand = 10
+    sim.operand = 010
     sim.branch_zero()
     assert sim.instruction_counter == sim.operand
 
 def test_execute_op_code_42_no_branch():
     sim = UVSim()
     sim.accumulator = 5
-    sim.operand = 10
+    sim.operand = 010
     sim.branch_zero()
     assert sim.instruction_counter != sim.operand
 
@@ -230,14 +231,14 @@ def test_reading_in_filename_2():
     filename = "Test Files/Test2.txt"
     sim.load(filename)
     # Assert the desired behavior based on the loaded file
-    expected_memory = [+1009,+1010,+2009,+3110,+4107,+1109,+4300,+1110,+4300,+0000,+0000,-99999]
+    expected_memory = [+01009,+01010,+02009,+03110,+04107,+01109,+04300,+01110,+04300,+00000,+00000,-099999]
     assert sim.memory[:len(expected_memory)] == expected_memory, f'Expected {expected_memory}, but got {sim.memory[:len(expected_memory)]}'
 
 #UT27
 def test_load_data_type_text1():
     sim = UVSim()
     sim.load('Test Files/Test1.txt')
-    expected_memory = [+1007, +1008, +2007, +2008, +2109, +1109, +4300, +0000, +0000, +0000, -99999]
+    expected_memory = [+01007, +01008, +02007, +02008, +02109, +01109, +04300, +00000, +00000, +00000, -099999]
 
     assert sim.memory[:len(expected_memory)] == expected_memory, f'Expected {expected_memory}, but got {sim.memory[:len(expected_memory)]}'
     assert isinstance(sim.memory, list)
@@ -248,6 +249,40 @@ def test_load_data_type_text2():
     filename = "Test Files/Test2.txt"
     sim.load(filename)
     # Assert the desired behavior based on the loaded file
-    expected_memory = [+1009,+1010,+2009,+3110,+4107,+1109,+4300,+1110,+4300,+0000,+0000,-99999]
+    expected_memory = [+01009,+01010,+02009,+03110,+04107,+01109,+04300,+01110,+04300,+00000,+00000,-099999]
     assert sim.memory[:len(expected_memory)] == expected_memory, f'Expected {expected_memory}, but got {sim.memory[:len(expected_memory)]}'
     assert isinstance(sim.memory, list)
+
+
+class TestUVSim(unittest.TestCase):
+    def setUp(self):
+        self.sim = UVSim(None, None, None, None)
+        self.test_file = "test_file.txt"
+        self.output_file = "test_file_converted.txt"
+
+        # Create a test input file
+        with open(self.test_file, 'w') as file:
+            file.write('+123\n-456\n789')
+
+    def tearDown(self):
+        # Clean up test files
+        os.remove(self.test_file)
+        if os.path.exists(self.output_file):
+            os.remove(self.output_file)
+
+    def test_convert_file(self):
+        converted_filename = self.sim.convert_file(self.test_file)
+
+        # Validate the filename
+        self.assertEqual(converted_filename, self.output_file)
+
+        # Validate the file content
+        with open(converted_filename, 'r') as file:
+            lines = file.read().split('\n')
+            self.assertEqual(lines[0], '+00123')
+            self.assertEqual(lines[1], '-00456')
+            self.assertEqual(lines[2], '000789')
+
+if __name__ == '__main__':
+    unittest.main()
+
--- myClasses/UVSim_Class.py
+++ myClasses/UVSim_Class.py
@@ -1,14 +1,8 @@
 import os
 
 class UVSim:
-
     def __init__(self, output_function, output_accumulator, output_instruction_process, handle_user_input):
-        self.memory = [0] * 250
-        self.accumulator = 0
-        self.instruction_counter = 0
-        self.instruction_register = 0
-        self.operation_code = 0
-        self.operand = 0
+        self.instances = {}  # Instances of UVSim for each file
         self.output_instruction_process = output_function
         self.output_accumulator = output_accumulator
         self.user_input_from_gui = 0
@@ -21,28 +15,38 @@ class UVSim:
             print(f"File '{filename}' does not exist.")
             return
 
+        memory = [0] * 250
         with open(filename, 'r') as file:
             for i, line in enumerate(file):
                 if i >= 250:
                     raise ValueError("File contains more than 250 lines.")
                 sign = -1 if line[0] == '-' else 1
-                self.memory[i] = int(line[1:]) * sign
+                memory[i] = int(line[1:]) * sign
 
-    def fetch(self):
+        self.instances[filename] = {
+            'memory': memory,
+            'accumulator': 0,
+            'instruction_counter': 0,
+            'instruction_register': 0,
+            'operation_code': 0,
+            'operand': 0
+        }
+
+    def fetch(self, filename):
         '''Fetch Instructions From Memory'''
-        # Read the instruction from memory at the location specified by the instruction counter
-        instruction_line = self.memory[self.instruction_counter]
-        # Convert the instruction line (a string) to an integer
+        data = self.instances[filename]
+        instruction_line = data['memory'][data['instruction_counter']]
         instruction = int(instruction_line)
-        # Operation code is the first three digits of the instruction
-        self.operation_code = instruction // 100
-        # Operand is the last two digits of the instruction
-        self.operand = instruction % 100
-        #print(f"Instruction Register: {self.instruction_register}")
+        data['operation_code'] = instruction // 100
+        data['operand'] = instruction % 100
 
-    def set_user_input(self, user_input):
-        self.user_input_from_gui = user_input
+    def set_user_input(self, filename, user_input):
+        self.instances[filename]['user_input_from_gui'] = user_input
 
+    def close(self, filename):
+        '''Remove a file and its program data from the dictionary'''
+        if filename in self.instances:
+            del self.instances[filename]
 
     def convert_file(self, filename):
         '''Converts 4-digit file to 6-digit file format'''
@@ -53,7 +57,7 @@ class UVSim:
         converted_lines = []
         with open(filename, 'r') as file:
             for line in file:
-                line = line.strip()  # remove newline characters
+                line = line.strip()
                 if line[0] in ['+', '-']:  # if the line starts with '+' or '-'
                     sign = line[0]
                     converted_lines.append(sign + line[1:].zfill(5))
@@ -66,105 +70,76 @@ class UVSim:
             file.write('\n'.join(converted_lines))
         return new_filename
 
-
     def execute(self, filename):
         '''Execute Instructions'''
-        self.load(filename)
+        if filename not in self.instances:
+            self.load(filename)
+        data = self.instances[filename]
         while True:
-            self.fetch()
-            if self.operation_code == int('043', 10): # HALT
-                '''Halt the program'''
+            self.fetch(filename)
+            if data['operation_code'] == int('043', 10): # HALT
                 self.output_instruction_process("Program halted.")
                 break  
-            elif self.operation_code == int('010', 10):  # READ
-                '''Gets user input and stores it in memory specified by the operand value'''
+            elif data['operation_code'] == int('010', 10):  # READ
                 self.output_instruction_process("Waiting for user input...")
-                # Call the handle_user_input method provided by the GUI to wait for user input
-                self.handle_user_input()
-                if self.user_input_from_gui is not None:
-                    self.memory[self.operand] = self.user_input_from_gui
+                self.handle_user_input(filename)
+                if 'user_input_from_gui' in data and data['user_input_from_gui'] is not None:
+                    data['memory'][data['operand']] = data['user_input_from_gui']
 
-            elif self.operation_code == int('011', 10): # WRITE
-                '''Write a word from a specific location in memory to screen'''
-                self.output_instruction_process("Triggered WRITE, operand 011")
-                # print(f"Memory[{self.operand}]: {self.memory[self.operand]}")
+            elif data['operation_code'] == int('011', 10): # WRITE
+                self.output_instruction_process(f"Triggered WRITE, operand {data['operand']}")
+                self.output_accumulator(data['memory'][data['operand']])
 
-            elif self.operation_code == int('020', 10):  # LOAD
-                '''Load a word from a specific location in memory into the accumulator'''
-                self.output_instruction_process("Triggered LOAD, operand 020")
-                self.accumulator = self.memory[self.operand]
-                self.output_accumulator(self.accumulator)
+            elif data['operation_code'] == int('020', 10):  # LOAD
+                self.output_instruction_process(f"Triggered LOAD, operand {data['operand']}")
+                data['accumulator'] = data['memory'][data['operand']]
+                self.output_accumulator(data['accumulator'])
 
-            elif self.operation_code == int('021', 10):  # STORE
-                '''Store a word from the accumulator into a specific location in memory'''
-                self.output_instruction_process("Triggered STORE, operand 021")
-                self.memory[self.operand] = self.accumulator
+            elif data['operation_code'] == int('021', 10):  # STORE
+                self.output_instruction_process(f"Triggered STORE, operand {data['operand']}")
+                data['memory'][data['operand']] = data['accumulator']
 
-            elif self.operation_code == int('030', 10):  # ADD
-                '''Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                self.output_instruction_process("Triggered ADD, operand 030")
-                self.accumulator += self.memory[self.operand]
+            elif data['operation_code'] == int('030', 10):  # ADD
+                self.output_instruction_process(f"Triggered ADD, operand {data['operand']}")
+                data['accumulator'] += data['memory'][data['operand']]
+                self.output_accumulator(data['accumulator'])
 
-            elif self.operation_code == int('031', 10): # SUBTRACT
-                '''Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)'''
-                self.output_instruction_process("Triggered SUBTRACT, operand 031")
-                self.accumulator -= self.memory[self.operand]
+            elif data['operation_code'] == int('031', 10): # SUBTRACT
+                self.output_instruction_process(f"Triggered SUBTRACT, operand {data['operand']}")
+                data['accumulator'] -= data['memory'][data['operand']]
+                self.output_accumulator(data['accumulator'])
 
-            elif self.operation_code == int('032', 10):  # DIVIDE
-                '''Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator)'''
-                self.output_instruction_process("Triggered DIVIDE, operand 032")
-                if self.memory[self.operand] != 0:
-                    self.accumulator /= self.memory[self.operand]
+            elif data['operation_code'] == int('032', 10):  # DIVIDE
+                self.output_instruction_process(f"Triggered DIVIDE, operand {data['operand']}")
+                if data['memory'][data['operand']] != 0:
+                    data['accumulator'] /= data['memory'][data['operand']]
+                    self.output_accumulator(data['accumulator'])
                 else:
                     raise ZeroDivisionError("Cannot divide by zero")
-                
-            elif self.operation_code == int('033', 10):# MULTIPLY
-                '''Multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)'''
-                self.output_instruction_process("Triggered MULTIPLY, operand 033")
-                self.accumulator *= self.memory[self.operand]
 
-            elif self.operation_code == int('040', 10):# BRANCH
-                '''Branch to a specific location in memory'''
-                self.output_instruction_process("Triggered BRANCH, operand 040")
-                self.instruction_counter = self.operand
-                
-            elif self.operation_code == int('041', 10): # BRANCHNEG
-                '''Branch to a specific location in memory if the accumulator is negative'''
-                self.output_instruction_process("Triggered BRANCHNEG, operand 041")
-                if self.accumulator < 0:
-                    self.instruction_counter = self.operand
+            elif data['operation_code'] == int('033', 10): # MULTIPLY
+                self.output_instruction_process(f"Triggered MULTIPLY, operand {data['operand']}")
+                data['accumulator'] *= data['memory'][data['operand']]
+                self.output_accumulator(data['accumulator'])
+
+            elif data['operation_code'] == int('040', 10): # BRANCH
+                self.output_instruction_process(f"Triggered BRANCH, operand {data['operand']}")
+                data['instruction_counter'] = data['operand']
+                continue
+
+            elif data['operation_code'] == int('041', 10): # BRANCHNEG
+                self.output_instruction_process(f"Triggered BRANCHNEG, operand {data['operand']}")
+                if data['accumulator'] < 0:
+                    data['instruction_counter'] = data['operand']
                     continue
-                
-            elif self.operation_code == int('042', 10): # BRANCHZERO
-                '''Branch to a specific location in memory if the accumulator is zero'''
-                self.output_instruction_process("Triggered BRANCHZERO, operand 042")
-                if self.accumulator == 0:
-                    self.instruction_counter = self.operand
+
+            elif data['operation_code'] == int('042', 10): # BRANCHZERO
+                self.output_instruction_process(f"Triggered BRANCHZERO, operand {data['operand']}")
+                if data['accumulator'] == 0:
+                    data['instruction_counter'] = data['operand']
                     continue
+
             else:
                 self.output_instruction_process("Invalid operation code.")
-        
-            self.instruction_counter += 1
 
-'''    
-def output_function(message):
-    print(message)
-
-def output_accumulator(value):
-    print("Accumulator:", value)
-
-def output_instruction_process(message):
-    print("Instruction Process:", message)
-
-def handle_user_input():
-    return 10
-
-# Instantiate UVSim
-sim = UVSim(output_function, output_accumulator, output_instruction_process, handle_user_input)
-
-# Convert the file to 6-digit format
-new_filename = sim.convert_file('Test1.txt')
-
-# Load and execute the converted file
-sim.execute('Test1_converted.txt')
-'''
+            data['instruction_counter'] += 1
